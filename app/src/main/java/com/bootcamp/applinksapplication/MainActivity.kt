4b5002ae3f4adf6ae025994ad2e3c9712a3569a1
Puneet Grover <ptgr.mail@gmail.com>
2022-03-23 11:32:44 +0530

    Android App Links Application

---
+++ app/src/main/java/com/bootcamp/applinksapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.bootcamp.applinksapplication
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // ATTENTION: This was auto-generated to handle app links.
+        val appLinkIntent = intent
+        val appLinkAction = appLinkIntent.action
+        val appLinkData = appLinkIntent.data
+    }
+}
---
+++ app/src/main/java/com/bootcamp/applinksapplication/MainActivity2.java
@@ -0,0 +1,20 @@
+package com.bootcamp.applinksapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+    }
+}
